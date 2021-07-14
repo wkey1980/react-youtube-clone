b5bbf70cdb9cb8f88fe3bf050439dff24fa148b0
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-14 11:06:56 +0100

    Building the header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,58 @@ import styled from 'styled-components'
 const Header = () => {
     return (
         <Container>
-            <h1>Header Section</h1>
+            <LeftMenu>
+                <Hamburger src="/images/icons/icon-hamburger.svg" alt="Menu Icon" />
+                <Logo src="/images/logo/logo.svg" alt="logo" />
+                <p>GB</p>
+            </LeftMenu>
+            <CenterMenu>
+                <Search>
+                    <input type="text" placeholder="Search" />
+                </Search>
+            </CenterMenu>
+            <RightMenu>
+                <h1>Right</h1>
+            </RightMenu>
         </Container>
     )
 }
+
+// Container
 const Container = styled.section`
     width: 100vw;
     height: 60px;
     background-color: lightcoral;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 0 25px;
 `;
+
+// Left Menu
+const LeftMenu = styled.div`
+    display: flex;
+
+    p {
+        vertical-align: sub;
+        font-size: 10px;
+        color: #606060;
+        margin-left: 5px;
+    }
+`;
+const Hamburger = styled.img``;
+const Logo = styled.img``;
+
+
+
+// Search
+const CenterMenu = styled.div``;
+const Search = styled.div``;
+// Tools
+const RightMenu = styled.div``;
 
 
 
 export default Header
-
-
-
-
-// 1) subscribe
-// 2) history
-// 3) Signin
